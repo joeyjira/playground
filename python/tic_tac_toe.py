6b1f9cedae510b885eccef30fd360a07cd176860
Joey Jirasevijinda <joey.jira@gmail.com>
2017-10-26 20:25:23 -0700

    enter moves into tic-tac-toe

--- python/tic_tac_toe.py
+++ python/tic_tac_toe.py
@@ -11,6 +11,17 @@ def print_board(board):
     print('--+--+--')
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
+turn = 'X'
+for i in range(9):
+    print_board(the_board)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    the_board[move] = ' ' + turn + ' '
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+
 print_board(the_board)
 
 
