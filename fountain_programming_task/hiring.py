e9851fbe0457b898b7d33e0983b4f393924924c1
Joey Jirasevijinda <joey.jira@gmail.com>
2017-10-30 11:51:06 -0700

    readlines for file

--- fountain_programming_task/hiring.py
+++ fountain_programming_task/hiring.py
@@ -1,18 +1,33 @@
 import os
 import sys
 
-def read_file():
+def read_file(filename):
     current_dir = os.getcwd()
-    if len(sys.argv) < 2:
-        print('Please enter an input file as an argument!')
-        return None
 
-    file_to_open = os.path.join(current_dir, sys.argv[1])
+
+    file_to_open = os.path.join(current_dir, filename)
     if os.path.exists(file_to_open):
         input_file = open(file_to_open)
-        file_content = input_file.read()
-        print(file_content)
+        file_content = input_file.readlines()
     else:
         print('File entered does not exist!')
 
-read_file()
+def write_file():
+    print('Write File')
+
+def check_file():
+    print('Check file')
+
+def hiring():
+    # Read in the file
+    if len(sys.argv) < 2:
+        print('Please enter an input file as an argument!')
+        return None
+    content = read_file(sys.argv[1])
+
+    # Check the file against mock database
+    mock_database = read_file('mock_database.txt')
+
+    # Write to output file
+
+hiring()
