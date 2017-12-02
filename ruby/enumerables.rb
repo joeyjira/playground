34db3de5fb5e0003b400d56296bb8da3f4255e07
Joey Jirasevijinda <joey.jira@gmail.com>
2017-12-01 20:26:52 -0800

    my_select

--- ruby/enumerables.rb
+++ ruby/enumerables.rb
@@ -3,6 +3,21 @@ class Array
         i = 0
         while i < self.length
             prc.call(self[i])
+            i += 1 
         end 
+        return self
+    end
+
+    def my_select(&prc)
+        result = []
+        self.my_each do |el|
+            result.push(el) if prc.call(el)
+        end 
+
+        return result
     end
 end
+
+array = [1, 2, 3, 4]
+
+puts array.my_select { |el| el.even? }
