f3cbc4be9d1d9006628bbdf182446ef1323a0f2d
Joey Jirasevijinda <joey.jira@gmail.com>
2017-12-03 23:29:11 -0800

    Add Point

--- ruby/ghost/game.rb
+++ ruby/ghost/game.rb
@@ -2,22 +2,29 @@ require "set"
 
 class Game
     def initialize(*players)
-        @players = players
-        @fragment = ""
         words = File.readlines("dictionary.txt").map(&:chomp)
+        @players = players
         @dictionary = Set.new(words)
     end 
 
     def play_round
+        @fragment = ""
         is_word = false
         until is_word 
             puts "Enter a letter: "
             letter = gets.chomp
-            @fragment += letter
-            puts "Your String is now: #{@fragment}"
-            is_word = true if @dictionary.include?(@fragment)
+            if valid_word?(word)
+                @fragment << letter
+            else
+                puts "Please enter a valid letter"
+                next
+            end
         end 
-    end 
+    end
+
+    def valid_word?(word)
+        @dictionary.include?(word)
+    end
 end
 
 game = Game.new
--- ruby/ghost/player.rb
+++ ruby/ghost/player.rb
@@ -2,5 +2,9 @@ class Player
     def initialize(name)
         @name = name
         @wins = 0
+    end
+
+    def add_point
+        @wins += 1
     end 
 end
