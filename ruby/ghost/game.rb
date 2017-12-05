62e02cfb16bd1d5c09eeda560296b85b7f831366
Joey Jirasevijinda <joey.jira@gmail.com>
2017-12-04 23:51:18 -0800

    tracks current player

--- ruby/ghost/game.rb
+++ ruby/ghost/game.rb
@@ -5,16 +5,21 @@ class Game
         words = File.readlines("dictionary.txt").map(&:chomp)
         @players = players
         @dictionary = Set.new(words)
+        @current_player = @players[0]
     end 
 
     def play_round
         @fragment = ""
         is_word = false
+        i = 0
         until is_word 
             puts "Enter a letter: "
             letter = gets.chomp
             if valid_word?(word)
                 @fragment << letter
+                @current_player = @players[i]
+                i += 1 
+                i = 0 if i > @players.length - 1
             else
                 puts "Please enter a valid letter"
                 next
