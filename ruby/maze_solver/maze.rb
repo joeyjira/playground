9658fd2c0063225ccd80e13c8a5eed4a988aa031
Joey Jirasevijinda <joey.jira@gmail.com>
2017-12-06 21:49:37 -0800

    read maze as array

--- ruby/maze_solver/maze.rb
+++ ruby/maze_solver/maze.rb
@@ -1,8 +1,12 @@
 class Maze
     def initialize(path)
-        @maze = File.open(path)
+        @maze = File.readlines('maze.txt')
     end
 
-    def read_path
+    def find_start
+        result = []
+        @maze.each_index do |line_index|
+            next unless line_index.include?("S")
+        end 
     end
 end 
