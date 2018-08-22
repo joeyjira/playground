a3875250428be4f1b79a6bbe75d81666243a63f8
Joey Jirasevijinda <joey.jira@gmail.com>
2018-08-22 08:24:08 -0700

    Added comments to describe program

--- C++/C++ Primer/Chapter 1/prog2.cc
+++ C++/C++ Primer/Chapter 1/prog2.cc
@@ -1,9 +1,13 @@
 #include <iostream>
-
+/*
+ * Simple main function:
+ * Read two numbers and write their sum
+ */
 int main() {
+  // Prompt users to enter 2 numbers
   std::cout << "Enter Two Numbers:" << std::endl;
-  int v1 = 0, v2 = 0;
-  std::cin >> v1 >> v2;
+  int v1 = 0, v2 = 0;     // Variables to store input we read
+  std::cin >> v1 >> v2;   // Read input
   std::cout << "The sum of " << v1 << " and " << v2
             << " is " << v1 + v2 << std::endl;
 
