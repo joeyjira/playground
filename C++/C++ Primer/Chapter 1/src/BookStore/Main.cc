3639d00130592ba260804987a762a75cbd0ebc39
Joey Jirasevijinda <joey.jira@gmail.com>
2018-08-23 13:57:21 -0700

    Modify to check for book with same ISBN

--- C++/C++ Primer/Chapter 1/src/BookStore/Main.cc
+++ C++/C++ Primer/Chapter 1/src/BookStore/Main.cc
@@ -5,7 +5,13 @@ int main() {
   Sales_item item1, item2;
   // Read ISBN, number of copies sold, and sales price
   std::cin >> item1 >> item2;
-  // Write ISBN, number of copies sold, total revenue, and average price
-  std::cout << item1 + item2 << std::endl;
-  return 0;
+  // First check that item1 and item2 represent the same book
+  if (item1.isbn() == item2.isbn()) {
+    std::cout << item1 + item2 << std::endl;
+    return 0;   // Indicate success
+  } else {
+    std::cerr << "Data must refer to same ISBN"
+              << std::endl;
+    return -1;  // Indicate failure
+  }
 }
